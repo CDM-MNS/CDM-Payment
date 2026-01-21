0825097264cfc6fa21bfa713d36eccb777df28b9
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-01-21 11:43:45 +0100

    feat: npm package implementation

--- src/Controllers/payment.controller.ts
+++ src/Controllers/payment.controller.ts
@@ -2,12 +2,13 @@ import {Controller} from "@nestjs/common";
 import {PaymentService} from "../Services/payment.service";
 import {MessagePattern, Payload} from "@nestjs/microservices";
 import {RpcValidationPipe} from "../Utils/rpc-validation";
+import {WalletEventType} from "cdm-models";
 
 @Controller('payment')
 export class PaymentController {
     constructor(private paymentService: PaymentService) {}
 
-    @MessagePattern({cmd: 'wallet.addMoneyToWallet'})
+    @MessagePattern({cmd: WalletEventType.ADD_MONEY})
     async makePayment(@Payload(new RpcValidationPipe()) payload: { amountToAdd: number }){
         return await this.paymentService.makePayment(payload.amountToAdd);
     }
